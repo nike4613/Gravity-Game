9569155da31f273b3e3c2315ecadc52c93f2dd96
aaron <aaron.cirr.com@gmail.com>
2015-03-20 21:51:17 -0500

    Finished CustomFont loading

--- GravityGame/src/net/mc42/games/ImageUtils.java
+++ GravityGame/src/net/mc42/games/ImageUtils.java
@@ -2,12 +2,13 @@ package net.mc42.games;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.util.ArrayList;
+
+import net.mc42.global.ByteString;
 
 import org.newdawn.slick.Image;
 
 public class ImageUtils {
-	public static Image getImageFromBytes(ArrayList<Byte> image) throws Exception{
+	public static Image getImageFromBytes(ByteString image) throws Exception{
 		File f = new File(System.getProperty("user.dir") + "/tmp/decoder.png");
 		File f1 = new File((System.getProperty("user.dir") + "/tmp"));
 		if(!f.isFile() || !f1.isDirectory()){
@@ -15,8 +16,7 @@ public class ImageUtils {
 			f.createNewFile();
 		}
 		FileOutputStream fo = new FileOutputStream(f);
-		for(byte b:image)
-			fo.write(b);
+		fo.write(image.toByteArray());
 		fo.close();
 		return new Image(System.getProperty("user.dir") + "/tmp/decoder.png");
 	}
--- GravityGame/src/net/mc42/games/fonts/CustomFont.java
+++ GravityGame/src/net/mc42/games/fonts/CustomFont.java
@@ -8,6 +8,7 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import net.mc42.games.ImageUtils;
+import net.mc42.global.ByteString;
 import net.mc42.global.Global;
 import net.mc42.global.Pair;
 
@@ -25,40 +26,40 @@ public class CustomFont {
 		loadFontFile(fontfile);
 	}
 	
-	@SuppressWarnings("unchecked")
 	private void loadFontFile(String filename) throws Exception{
 		ZipInputStream file = new ZipInputStream(/*new FileInputStream*/(getClass().getResourceAsStream(filename)));
 		ZipEntry e;
 		byte[] rdbuffer = new byte[1024];
-		ArrayList<Byte> buffer = new ArrayList<Byte>();
-		ArrayList<Byte> image = new ArrayList<Byte>();
-		String text = new String();
+		ByteString buffer = new ByteString();
+		ByteString image = new ByteString();
+		String text = "";
 		while((e = file.getNextEntry()) != null){
-			buffer = new ArrayList<Byte>();
+			buffer = new ByteString();
 			Global.log(Global.levels.DEBUG, "Found file " + e.getName() + " in font file " + filename);
 			int len = 0;
 			while((len = file.read(rdbuffer))>0){
-				for(byte b:rdbuffer)
-					buffer.add(b);
-				Global.log(Global.levels.DEBUG, "Read " + len + " bytes from file " + filename + "!" + e.getName() + " -- " + new String(rdbuffer));
+				/*for(byte b:rdbuffer)
+					buffer.add(b);*/
+				buffer.add(rdbuffer);
+				Global.log(Global.levels.DEBUG, "Read " + len + " bytes from file " + filename + "!" + e.getName());
+				//Global.log(Global.levels.DEBUG, buffer.toString());
 			}
-			if(e.getName() == "icons.png"){
-				image = (ArrayList<Byte>) buffer.clone();
+			if(e.getName().endsWith(".png")){
+				image = buffer;
 			}
-			if(e.getName() == "charmap.map"){
-				for(byte b:buffer)
-					text += (char)b;
+			if(e.getName().endsWith(".map")){
+				text = buffer.toString();
 			}
-			Global.log(Global.levels.DEBUG, text);
+			//Global.log(Global.levels.DEBUG, text);
 		}
 		name = filename;
 		file.close();
-		Global.log(Global.levels.DEBUG, text);
+		//Global.log(Global.levels.DEBUG, text);
 		loadCharacters(text);
 		loadImages(image);
 	}
 	
-	private void loadImages(ArrayList<Byte> image) throws Exception{
+	private void loadImages(ByteString image) throws Exception{
 		Image i = ImageUtils.getImageFromBytes(image);
 		if(i.getWidth()!=i.getHeight()&&(i.getWidth()%16)!=0) 
 			throw new FontLoadException("The image file for font " + name + " is incorrectly proportioned!");
@@ -67,7 +68,7 @@ public class CustomFont {
 	
 	private void loadCharacters(String text) throws Exception{
 		List<String> lines = new ArrayList<>();
-		Global.log(Global.levels.DEBUG, "Text: " + text);
+		//Global.log(Global.levels.DEBUG, "Text: " + text);
 		while(text.indexOf('\n')>0){
 			lines.add(text.substring(0, text.indexOf('\n')).trim());
 			text = text.substring(text.indexOf('\n')).trim();
